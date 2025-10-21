bbc9651621cd06834d1fd46f9a3ccff2d7e1ece9
Josem27 <126608382+Josem27@users.noreply.github.com>
2025-10-21 16:26:19 +0200

    Inicio
    Inicializa proyecto Vue 3 con Vite y limpia estructura base

---
+++ tiempo-app/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Weather App</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style>
+body {
+  margin: 0;
+  font-family: system-ui, sans-serif;
+  background: linear-gradient(135deg, #89f7fe, #66a6ff);
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
